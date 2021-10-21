e58b115ef3160bcd3d51cd6696fb2c233553d0d2
zaynab <zjquery66@gmail.com>
2021-10-21 15:20:41 -0700

    added space Id

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       resolve: "gatsby-source-contentful",
       options: {
         accessToken: "PBZzgI-TME10TyezKFL00qjWAp8swxsPioAMDZOjM3U",
-        spaceId: "",
+        spaceId: "zftkd6qhq6hg",
       },
     },
     "gatsby-plugin-styled-components",
